739c2a8f41736ce777a8366b557e23359ecadfa6
Matheus Kaczan <matheuskp29@gmail.com>
2021-06-02 16:39:27 -0300

    Created project initial sctruture

---
+++ src/test/java/one/digitalInnovation/personaapi/PersonaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalInnovation.personaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
