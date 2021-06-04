a15c18272adf3630405e8b43716253b71d23ade4
Matheus Kaczan <matheuskp29@gmail.com>
2021-06-04 10:24:37 -0300

    Developed person create operation

---
+++ src/main/java/one/digitalInnovation/personaapi/service/PersonService.java
@@ -0,0 +1,25 @@
+package one.digitalInnovation.personaapi.service;
+
+import one.digitalInnovation.personaapi.dto.response.MessageResponseDTO;
+import one.digitalInnovation.personaapi.entity.Person;
+import one.digitalInnovation.personaapi.repository.PersonRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+public class PersonService {
+
+    private PersonRepository personRepository;
+
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    public MessageResponseDTO createPerson(Person person){
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + savedPerson.getId())
+                .build();
+    }
+}
